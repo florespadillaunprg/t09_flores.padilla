4c1ee207b78659786c0408a6241c03dc965cb693
florespadillaunprg <57303134+florespadillaunprg@users.noreply.github.com>
2019-12-20 10:21:01 -0500

    Se agrego archivo lote.bat

---
+++ lote.bat
@@ -0,0 +1,49 @@
+python flores/funciones01.py "5" "5"
+
+python flores/funciones02.py "3" "6" "10"
+
+python flores/funciones03.py "12" "12" "12" "12"
+
+python flores/funciones04.py "3.14" "20"
+
+python flores/funciones05.py "100" "20" "12"
+
+python flores/funciones06.py "25" "12" "2"
+
+python flores/funciones07.py "25" "10" "5"
+
+python flores/funciones08.py "25" "5" "65"
+
+python flores/funciones09.py "40" "50" "60"
+
+python flores/funciones10.py "5" "78" "45"
+
+python flores/funciones11.py "Andres" "Martin" "Vizcarra" "Cornejo"
+
+python flores/funciones12.py "Alex" "Flores Padilla" "645.2"
+
+python flores/funciones13.py "a" "5" "6" "5" "2.3" "2.51" "9.01"
+
+python flores/funciones14.py "45" "5" "3" "12"
+
+python flores/funciones15.py "2.3" "45.2" "85.01"
+
+python flores/funciones16.py "5" "56" "52"
+
+python flores/funciones17.py "40" "500" "302" "98"
+
+python flores/funciones18.py "45" "55" "3" "98"
+
+python flores/funciones19.py "605" "55"
+
+python flores/funciones20.py "45" "55" "34"
+
+python flores/funciones21.py "495" "125" "34" "98"
+
+python flores/funciones22.py "415" "505" "34"
+
+python flores/funciones23.py "2" "87" "98" "256" "9"
+
+python flores/funciones24.py "2" "87" "98" "256" "25" "30"
+
+python flores/funciones25.py "2" "87" "98" "256" "25"
